f20fa90937aad15787b682154cd335f5db2d0d67
GentleZhu <544524199@qq.com>
2018-12-29 11:26:32 -0600

    Add contrastive analysis between target and twin

---
+++ models/summarizer.py
@@ -0,0 +1,164 @@
+from IPython import embed
+import numpy as np
+import sys
+from tqdm import tqdm
+import random
+sys.path.append('../')
+
+from collections import defaultdict
+import pickle
+from phraseExtractor import phraseExtractor
+
+def leven_similarity(phrase_a, phrase_b):
+    # Also known as 'edit distance'
+    len1, len2 = len(phrase_a), len(phrase_b)
+    #if not (phrase_a[:5] == phrase_b[:5] or phrase_a[-5:] == phrase_b[-5:]):
+    #    return 10
+    matrix = [[0 for _ in range(len2)] for _ in range(len1)]
+    for i in range(len1):
+        for j in range(len2):
+            if i == 0 or j == 0:
+                matrix[i][j] = max(i, j)
+            else:
+                if phrase_b[j] != phrase_a[i]:
+                    matrix[i][j] = min([matrix[i - 1][j] + 1, matrix[i][j - 1] + 1, matrix[i - 1][j - 1] + 1])
+                else:
+                    matrix[i][j] = min([matrix[i - 1][j] + 1, matrix[i][j - 1] + 1, matrix[i - 1][j - 1]])
+    return matrix[len1 - 1][len2 - 1]
+
+def select_phrases(relevance_score, similarity_score, weight, k):
+    # Diversified ranking.
+    # relevance score: 1-d numpy array. similarity score: 2-d numpy array
+    # weight: >= 2 to ensure monotonity.
+    reference_vector = np.dot(relevance_score, similarity_score)
+    #reference_vector = np.ones(relevance_score.shape[0])
+    score = weight * (reference_vector * relevance_score) - np.diag(similarity_score) * relevance_score * relevance_score
+    selected_indices = set()
+    for i in range(k):
+        current_max = -1
+        ret = 0
+        for idx, x in enumerate(score):
+            if idx in selected_indices:
+                continue
+            if x > current_max:
+                current_max = x
+                ret = idx
+        selected_indices.add(ret)
+        score = score - 2 * relevance_score[ret] * (similarity_score[:, ret] * relevance_score)
+    return selected_indices
+
+def collect_statistics(in_file='/shared/data/qiz3/text_summ/src/jt_code/doc2cube/tmp_data/sports.txt'):
+    document_phrases = defaultdict(lambda: defaultdict(int))
+    inverted_index = defaultdict(lambda: defaultdict(int))
+    with open(in_file) as IN:
+        for line in IN:
+            line = line.strip('\n').split('\t')
+            if line[1] != '':
+                doc_id, phrases = int(line[0]), line[1].split(';')
+            else:
+                continue
+            for phrase in phrases:
+                document_phrases[doc_id][phrase] += 1
+                inverted_index[phrase][doc_id] += 1
+    return document_phrases, inverted_index
+
+def load_doc_sets(sib_file='/shared/data/qiz3/text_summ/src/jt_code/doc2cube/src/sib.dump',
+                        twin_file='/shared/data/qiz3/text_summ/src/jt_code/doc2cube/src/twin.dump'):
+    siblings = pickle.load(open(sib_file, 'rb'))
+    twin_docs = pickle.load(open(twin_file, 'rb'))
+    return siblings, twin_docs
+
+def generate_candidate_phrases(document_phrase_cnt, docs):
+    # Given doc_ids, generate all candidate phrases.
+    phrase2idx = defaultdict()
+    for doc_id in docs:
+        for phrase in document_phrase_cnt[doc_id]:
+            if phrase not in phrase2idx:
+                phrase2idx[phrase] = len(phrase2idx)
+    return phrase2idx
+
+def generate_caseOLAP_scores(sibling_groups, target_set, document_phrase_cnt, inverted_index, phrase2idx):
+    phrase_candidates = list(phrase2idx.keys())
+    target_phrase_freq = defaultdict(int)
+    for idx in target_set:
+        for phrase in document_phrase_cnt[idx]:
+            target_phrase_freq[phrase] += document_phrase_cnt[idx][phrase]
+
+    phrase_extractor = phraseExtractor(phrase_candidates, phrase2idx, target_set, sibling_groups, target_phrase_freq)
+    ranked_list = phrase_extractor.compute_scores(document_phrase_cnt, inverted_index, 'A')
+    scores = np.array([0.0 for _ in range(len(ranked_list))])
+
+    for t in ranked_list:
+        scores[phrase2idx[t[0]]] = t[1]
+    return scores,ranked_list
+
+def calculate_pairwise_similarity(phrase2idx):
+    # phrase2idx: dict, {'USA':1, ... }
+    idx2phrase = {phrase2idx[k]:k for k in phrase2idx}
+    #similarity_scores = np.zeros([len(phrase2idx), len(phrase2idx)])
+    print('Calculate pairwise similarity...')
+    #for i in tqdm(range(len(phrase2idx))):
+    #    for j in range(len(phrase2idx)):
+    #        if j < i:
+    #            similarity_scores[i][j] = similarity_scores[j][i]
+    #        else:
+    #            similarity_scores[i][j] = 1.0 / (1 + leven_similarity(idx2phrase[i], idx2phrase[j]))
+    #similarity_scores = np.zeros([len(phrase2idx), len(phrase2idx)])
+    similarity_scores = np.eye(len(phrase2idx))
+    return similarity_scores, idx2phrase
+
+def contrastive_analysis(document_phrase_cnt, background_phrases, twin, target):
+    # Rerank phrases in target docs using twin docs.
+    # Currently use freq diff.
+    twin_cnt = defaultdict(int)
+    target_cnt = defaultdict(int)
+    for doc_id in twin:
+        for phrase in document_phrase_cnt[doc_id]:
+            twin_cnt[phrase] += document_phrase_cnt[doc_id][phrase]
+    for doc_id in target:
+        for phrase in document_phrase_cnt[doc_id]:
+            target_cnt[phrase] += document_phrase_cnt[doc_id][phrase]
+    phrase_rescore = {}
+    for phrase in background_phrases:
+        if phrase not in target_cnt:
+            continue
+        phrase_rescore[phrase] = 1.0 * target_cnt[phrase] / twin_cnt[phrase]
+    ranked_list = [(phrase, phrase_rescore[phrase]) for phrase in phrase_rescore]
+    ranked_list = sorted(ranked_list, key=lambda t: -t[1])
+    return ranked_list, phrase_rescore
+
+def random_sample_sibling(siblings, k):
+    # Randomly sample k docs for each sibling.
+    new_siblings = []
+    for sibling in siblings:
+        if len(sibling) <= k:
+            new_siblings.append(sibling)
+        else:
+            new_siblings.append(random.sample(sibling, k))
+    return new_siblings
+
+def main():
+    target_docs = [846, 845, 2394, 2904, 2633, 2565, 2956, 2728, 2491]
+    document_phrase_cnt, inverted_index = collect_statistics()
+    siblings, twin_docs = load_doc_sets()
+    phrase2idx = generate_candidate_phrases(document_phrase_cnt, twin_docs)
+    similarity_scores, idx2phrase = calculate_pairwise_similarity(phrase2idx)
+
+    #similarity_scores = pickle.load(open('similarity_score.p', 'rb'))
+    #idx2phrase = pickle.load(open('idx2phrase.p', 'rb'))
+
+    #siblings = random_sample_sibling(siblings, 10)
+
+    scores, ranked_list = generate_caseOLAP_scores(siblings, twin_docs, document_phrase_cnt, inverted_index, phrase2idx)
+
+    #scores = pickle.load(open('tmp_scores.dump','rb'))
+    #pickle.dump(scores, open('tmp_scores.dump','wb'))
+
+    selected_index = select_phrases(scores, similarity_scores, 2, 1000)
+    phrases = [idx2phrase[k] for k in selected_index]
+    ranked_list, phrase_rescore = contrastive_analysis(document_phrase_cnt, phrases, twin_docs, target_docs)
+    embed()
+    exit()
+
+if __name__ == '__main__':
+    main()
---
+++ phraseExtractor.py
@@ -0,0 +1,151 @@
+import numpy as np
+import math
+from IPython import embed
+from tqdm import tqdm
+
+class phraseExtractor:
+    def __init__(self, entity_candidates, phrase2idx, target_doc_list, sibling_groups, entity2freq):
+        self.entity_candidates = entity_candidates
+        self.target_doc_list = target_doc_list
+        self.sibling_groups = sibling_groups
+        self.phrase2idx = phrase2idx
+        self.entity2embed = None#entity2embed
+        self.entity2freq = entity2freq
+
+        self.ranked_list = []
+
+    def read_int(self, file_path):
+        self.int_score = {}
+        with open(file_path) as IN:
+            for line in IN:
+                score, phrase = line.strip('\n').split('\t')
+                score = float(score)
+                phrase = phrase.replace(' ', '_')
+                self.int_score[phrase] = score
+
+    def bm25_df_paper(self, df, max_df, tf, dl, avgdl, k=1.2, b=0.5, multiplier=3):
+        if df * tf == 0:
+            return 0
+        score = tf * (k + 1) / (tf + k * (1 - b + b * (dl / avgdl)))
+        df_factor = math.log(1 + df, 2) / math.log(1 + max_df, 2)
+        score *= df_factor
+        score *= multiplier
+        return score
+
+    def softmax_paper(self, score_list):
+        # normalization of exp
+        exp_sum = 1
+        for score in score_list:
+            exp_sum += math.exp(score)
+
+        exp_list = []
+        for score in score_list:
+            normal_value = math.exp(score) / exp_sum
+            exp_list.append(normal_value)
+        return exp_list
+
+    def _get_target_phrase_cnt(self, target_doc_ids):
+        s = 0
+        for doc_id in target_doc_ids:
+            s += sum([self.document_phrase_cnt[doc_id][entity] for entity in self.document_phrase_cnt[doc_id]])
+        return s
+
+    def _calculate_max_df(self, sibling_group):
+        print('Collecting group entities...')
+        entities = set()
+        for doc_id in tqdm(sibling_group):
+            entities |= self.document_phrase_cnt[doc_id].keys()
+        max_df = -1
+        print('Done. Calculating max df...')
+        for entity in tqdm(entities):
+            df = sum([1 if doc_id in self.inverted_index[entity] else 0 for doc_id in sibling_group])
+            if df > max_df:
+                max_df = df
+        print('Done.')
+        return max_df
+
+    def load_freq_data(self, document_phrase_cnt, inverted_index):
+        self.document_phrase_cnt = document_phrase_cnt
+        self.inverted_index = inverted_index
+
+        self.avg_dl = sum([self._get_target_phrase_cnt(sibling_cell) for sibling_cell in self.sibling_groups])
+        self.avg_dl += self._get_target_phrase_cnt(self.target_doc_list)
+        self.avg_dl /= (len(self.sibling_groups) + 1)
+
+    def _calculate_sibling_max_df(self):
+        max_dfs = [self._calculate_max_df(self.target_doc_list)]
+        for sibling in self.sibling_groups:
+            max_dfs.append(self._calculate_max_df(sibling))
+        self.max_dfs = max_dfs
+
+    def _calculate_tf(self, entity, group):
+        tf = sum([self.inverted_index[entity][doc_id] for doc_id in group])
+        return tf
+
+    def _compute_entity_score(self, entity, score_option):
+        current_df = sum([1 if self.inverted_index[entity][doc_id] > 0 else 0 for doc_id in self.target_doc_list])
+        score_list = []
+        context_group = [(self._get_target_phrase_cnt(self.target_doc_list), current_df)]
+        for sibling_group in self.sibling_groups:
+            df = sum([1 if self.inverted_index[entity][doc_id] > 0 else 0 for doc_id in sibling_group])
+            context_group.append((self._get_target_phrase_cnt(sibling_group), df))
+        for idx, g in enumerate(context_group):
+            if idx == 0:
+                score_list.append(self.bm25_df_paper(g[1], self.max_dfs[idx],
+                                                     self._calculate_tf(entity, self.target_doc_list), g[0], self.avg_dl))
+            else:
+                score_list.append(self.bm25_df_paper(g[1], self.max_dfs[idx],
+                                                     self._calculate_tf(entity, self.sibling_groups[idx - 1]), g[0], self.avg_dl))
+        score_list = self.softmax_paper(score_list)
+
+        distinctiveness = score_list[0]
+        popularity = math.log(1 + self.entity2freq[entity], 2)
+        if score_option == 'A':
+            score = popularity * distinctiveness
+        elif score_option == 'B':
+            score = distinctiveness
+        elif score_option == 'C':
+            score = popularity
+        elif score_option == 'D':
+            int_score = self.int_score[entity] if entity in self.int_score else 0.
+            score = popularity * distinctiveness * int_score
+        elif score_option == 'E':
+            int_score = self.int_score[entity] if entity in self.int_score else 0.3
+            score = int_score
+        elif score_option == 'F':
+            int_score = self.int_score[entity] if entity in self.int_score else 0.3
+            score = int_score * distinctiveness
+        elif score_option == 'H':
+            int_score = self.int_score[entity] if entity in self.int_score else 0.
+            score = int_score * popularity
+        else:
+            raise NotImplementedError
+        return score
+
+    def compute_scores(self, document_phrase_cnt, inverted_index, score_option):
+        # score_option: A. popularity+distinc;
+        #               B. distinc
+        #               C. popularity
+        #               D. Integrity + popularity + distinc
+        #               E. Integrity
+        #               F. Integrity + distinc
+        #               G. tf-idf
+        #               H. Integrity + popularity
+        self.load_freq_data(document_phrase_cnt, inverted_index)
+        self._calculate_sibling_max_df()
+        if score_option in ['D', 'E', 'F', 'H']:
+            self.read_int('/shared/data/qiz3/text_summ/src/jt_code/HiExpan-master/data/sports/intermediate/AutoPhrase_multi-words.txt')
+        print('Start calculating scores...')
+        for entity in tqdm(self.entity_candidates):
+            if score_option == 'G':
+                score = math.log(len(document_phrase_cnt)) / math.log(1 + len(inverted_index[entity])) * self.entity2freq[entity]
+                if '_' not in entity:
+                    score = 0.
+            else:
+                score = self._compute_entity_score(entity, score_option)
+                if '_' not in entity:
+                    score = 0.
+            self.ranked_list.append((entity, score))
+
+        self.ranked_list = sorted(self.ranked_list, key=lambda t: -t[1])
+        return self.ranked_list
